9a7e80bde4a202dd1a4450ba5ce4b46fd44127c4
wuamy <wuamyamy@gmail.com>
2016-08-22 13:52:43 -0400

    pom.xml

--- src/main/java/autoFramework/FirstTestCase.java
+++ src/main/java/autoFramework/FirstTestCase.java
@@ -1,5 +1,6 @@
 package autoFramework;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -20,12 +21,20 @@ public class FirstTestCase {
         System.out.println("Successfully opened the website");
 
         //login to a user account
+        driver.findElement(By.cssSelector(".account_icon")).click();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+
+        //enter user id and password
+        driver.findElement(By.cssSelector("#log")).sendKeys("amy");
+        driver.findElement(By.cssSelector("#pwd")).sendKeys("c3qKYLErI*IClnTO");
+        driver.findElement(By.cssSelector("#login")).click();
+
 
 
         //wait for 5 second
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //Close the driver
-        driver.quit();
+        //driver.quit();
     }
 }
