d1c0fe8c12cf0970bdfe5ea866289a1ec5f14270
wuamy <wuamyamy@gmail.com>
2016-08-26 15:29:28 -0400

    update the code

---
+++ src/main/java/utility/Constant.java
@@ -0,0 +1,12 @@
+package utility;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+/**
+ * Created by amy on 8/26/2016.
+ */
+public class Constant {
+    public static final String URL = "http://store.demoqa.com/";
+    public static final String Username = "amy";
+    public static final String Password ="c3qKYLErI*IClnTO";
+}
