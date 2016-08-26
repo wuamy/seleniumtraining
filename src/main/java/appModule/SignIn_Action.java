2ade3eae892e35be950e61965c6437bcf54d165f
wuamy <wuamyamy@gmail.com>
2016-08-26 15:12:30 -0400

    update the code

--- src/main/java/appModule/SignIn_Action.java
+++ src/main/java/appModule/SignIn_Action.java
@@ -6,11 +6,12 @@ import pageObjects.*;
  * Created by amy on 8/26/2016.
  */
 public class SignIn_Action {
-    public static void Execute(WebDriver driver){
+    public static void Execute(WebDriver driver, String sUname, String sPassword){
         Home_Page.lnk_MyAccount(driver).click();
-
-        LogIn_Page.txtbx_UserName(driver).sendKeys("amy");
-        LogIn_Page.txtbx_Password(driver).sendKeys("c3qKYLErI*IClnTO");
+        // enter user id and password through a variable
+        LogIn_Page.txtbx_UserName(driver).sendKeys(sUname);
+        //LogIn_Page.txtbx_Password(driver).sendKeys("c3qKYLErI*IClnTO");
+        LogIn_Page.txtbx_Password(driver).sendKeys(sPassword);
         LogIn_Page.btn_LogIn(driver).click();
     }
 }
