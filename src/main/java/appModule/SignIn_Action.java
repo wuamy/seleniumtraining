508b2cc8817d9161ffebe17a0a802e5ca5216e94
wuamy <wuamyamy@gmail.com>
2016-08-29 08:27:24 -0400

    update the code

--- src/main/java/appModule/SignIn_Action.java
+++ src/main/java/appModule/SignIn_Action.java
@@ -2,16 +2,21 @@ package appModule;
 
 import org.openqa.selenium.WebDriver;
 import pageObjects.*;
+import utility.Log;
 /**
  * Created by amy on 8/26/2016.
  */
 public class SignIn_Action {
     public static void Execute(WebDriver driver, String sUname, String sPassword){
         Home_Page.lnk_MyAccount(driver).click();
+        Log.info("Click actin performed on My account link");
         // enter user id and password through a variable
         LogIn_Page.txtbx_UserName(driver).sendKeys(sUname);
+        Log.info("Type in user login name");
         //LogIn_Page.txtbx_Password(driver).sendKeys("c3qKYLErI*IClnTO");
         LogIn_Page.txtbx_Password(driver).sendKeys(sPassword);
+        Log.info("Type in user login password");
         LogIn_Page.btn_LogIn(driver).click();
+        Log.info("Click submit button");
     }
 }
---
+++ src/main/java/autoFramework/Log4j_Logging_TC.java
@@ -0,0 +1,7 @@
+package autoFramework;
+
+/**
+ * Created by amy on 8/29/2016.
+ */
+public class Log4j_Logging_TC {
+}
