67199193b352573a86ec8b63703306b7055823dd
wuamy <wuamyamy@gmail.com>
2016-08-31 09:13:35 -0400

    update the code

--- src/main/java/utility/Listener.java
+++ src/main/java/utility/Listener.java
@@ -71,16 +71,60 @@ public class Listener implements ITestListener, ISuiteListener, IInvokedMethodLi
     public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
     }
 
-    //This is the method
+    //This is the method which will be executed in case of test pass or fail
+    //This will provide the information on the test
+    private void printTestResults(ITestResult result){
+        Reporter.log("Test method resides in " + result.getTestClass().getName(), true);
 
+        if (result.getParameters().length != 0){
+            String params = null;
+            for (Object parameter : result.getParameters()){
+                params += parameter.toString() + ",";
+            }
+            Reporter.log("Test Method had the following parameters : " + params, true);
+        }
 
+        String status = null;
+        switch (result.getStatus()){
+            case ITestResult.SUCCESS:
+                status = "Pass";
+                break;
 
+            case ITestResult.FAILURE:
+                  status = "Failed";
+                  break;
 
+            case ITestResult.SKIP:
+                  status = "Skipped";
+            }
+        Reporter.log("Test Status: " + status, true);
+    }
 
+    // This belongs to IInvokedMethodListener and will execute before every method including @Before @After @Test
 
+    public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
 
+        String textMsg = "About to begin executing following method : " + returnMethodName(arg0.getTestMethod());
 
+        Reporter.log(textMsg, true);
 
+    }
 
+    // This belongs to IInvokedMethodListener and will execute after every method including @Before @After @Test
 
+    public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
+
+        String textMsg = "Completed executing following method : " + returnMethodName(arg0.getTestMethod());
+
+        Reporter.log(textMsg, true);
+
+    }
+
+    // This will return method names to the calling function
+
+    private String returnMethodName(ITestNGMethod method) {
+
+        return method.getRealClass().getSimpleName() + "." + method.getMethodName();
+
+    }
 }
